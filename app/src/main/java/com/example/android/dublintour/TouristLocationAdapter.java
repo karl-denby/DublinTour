0a87dd2b202d16cf7c0936e2d53403501de53202
karl-denby <karl.denby@gmail.com>
2016-07-14 14:59:01 +0100

    listview adapter for locations

---
+++ app/src/main/java/com/example/android/dublintour/TouristLocationAdapter.java
@@ -0,0 +1,84 @@
+package com.example.android.dublintour;
+
+        import android.app.Activity;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.ArrayAdapter;
+        import android.widget.ImageView;
+        import android.widget.TextView;
+
+        import java.util.ArrayList;
+
+/*
+* {@link TouristLocationAdapter} is an {@link ArrayAdapter} that can provide the layout for each list
+* based on a data source, which is a list of {@link TouristLocation} objects.
+* */
+public class TouristLocationAdapter extends ArrayAdapter<TouristLocation> {
+
+    private static final String LOG_TAG = TouristLocationAdapter.class.getSimpleName();
+
+    /**
+     * This is our own custom constructor (it doesn't mirror a superclass constructor).
+     * The context is used to inflate the layout file, and the list is the data we want
+     * to populate into the lists.
+     *
+     * @param context        The current context. Used to inflate the layout file.
+     * @param touristLocations A List of TouristLocation objects to display in a list
+     */
+    public TouristLocationAdapter(Activity context, ArrayList<TouristLocation> touristLocations) {
+        // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
+        // the second argument is used when the ArrayAdapter is populating a single TextView.
+        // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
+        // going to use this second argument, so it can be any value. Here, we used 0.
+        super(context, 0, touristLocations);
+    }
+
+    /**
+     * Provides a view for an AdapterView (ListView, GridView, etc.)
+     *
+     * @param position The position in the list of data that should be displayed in the
+     *                 list item view.
+     * @param convertView The recycled view to populate.
+     * @param parent The parent ViewGroup that is used for inflation.
+     * @return The View for the position in the AdapterView.
+     */
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Check if the existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.listview_location, parent, false);
+        }
+
+        // Get the {@link TouristLocation} object located at this position in the list
+        TouristLocation currentTouristLocation = getItem(position);
+
+        // Find the ImageView in the listview_location.xml layout with the ID placeImage
+        ImageView placeImageView = (ImageView) listItemView.findViewById(R.id.placeImage);
+
+        // Get the place name from the current TouristLocation object and
+        // set this text on the name TextView
+        placeImageView.setImageResource(currentTouristLocation.getPlaceThumbnail());
+
+        // Find the TextView in the listview_location.xml layout with the ID placeName
+        TextView placeTextView = (TextView) listItemView.findViewById(R.id.placeName);
+
+        // Get the place name from the current TouristLocation object and
+        // set this text on the number TextView
+        placeTextView.setText(currentTouristLocation.getPlaceName());
+
+        // Find the TextView in the listview_location.xml layout with the ID placeDescription
+        TextView placeDescriptionView = (TextView) listItemView.findViewById(R.id.placeDescription);
+
+        // Get the place description from the current TouristLocation object and
+        // set this text on the number TextView
+        placeDescriptionView.setText(currentTouristLocation.getPlaceDescription());
+
+        // Return the whole list item layout (containing 2 TextViews and an ImageView)
+        // so that it can be shown in the ListView
+        return listItemView;
+    }
+
+}
