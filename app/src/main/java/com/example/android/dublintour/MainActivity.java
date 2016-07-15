273f930b83407c3d72c45727e1b6841484c926f8
Karl Denby <karl.denby@gmail.com>
2016-07-15 19:27:15 +0100

    location drawable and maps test

--- app/src/main/java/com/example/android/dublintour/MainActivity.java
+++ app/src/main/java/com/example/android/dublintour/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.android.dublintour;
 
+import android.content.Intent;
 import android.location.Location;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -18,10 +21,10 @@ public class MainActivity extends AppCompatActivity {
         // Create an ArrayList of TouristLocation objects
         ArrayList<TouristLocation> touristLocations = new ArrayList<TouristLocation>();
 
-        touristLocations.add(new TouristLocation("Place1", "1st place", "Dublin", R.mipmap.ic_launcher));
-        touristLocations.add(new TouristLocation("Place2", "2nd place", "Dublin"));
-        touristLocations.add(new TouristLocation("Place3", "3rd place", "Dublin"));
-        touristLocations.add(new TouristLocation("Place4", "4th place", "Dublin", R.mipmap.ic_launcher));
+        touristLocations.add(new TouristLocation("Kilmainham Jail", "Prison telling stories of Ireland's past", "geo: 53.3419423,-6.310682", R.mipmap.ic_launcher));
+        touristLocations.add(new TouristLocation("Guiness Storehouse", "Immersive brand experience & rooftop bar", "geo: 53.3418772,-6.288898"));
+        touristLocations.add(new TouristLocation("Collins Baracks", "Museum", "53.3479397,-6.2881163"));
+        touristLocations.add(new TouristLocation("Dublin Castle", "Museums, state rooms and Gothic chapel", "geo: 53.3445466,-6.2709349", R.mipmap.ic_launcher));
 
         // Create an {@link AndroidFlavorAdapter}, whose data source is a list of
         // {@link AndroidFlavor}s. The adapter knows how to create list item views for each item
@@ -33,4 +36,15 @@ public class MainActivity extends AppCompatActivity {
         listView.setAdapter(locationAdapter);
 
     }
+
+    public void showMap(View v) {
+        Uri geoLocation = Uri.parse("geo:47.6,-122.3");
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
 }
