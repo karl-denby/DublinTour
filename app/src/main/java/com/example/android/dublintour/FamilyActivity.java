1d001e381708c21c2283bb98a045b7cbe3337e56
Karl Denby <karl.denby@gmail.com>
2016-07-20 14:19:57 +0100

    Classes for family/landmark

---
+++ app/src/main/java/com/example/android/dublintour/FamilyActivity.java
@@ -0,0 +1,50 @@
+package com.example.android.dublintour;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import java.util.ArrayList;
+
+public class FamilyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_family);
+
+        // Create an ArrayList of TouristLocation objects
+        final ArrayList<TouristLocation> touristLocations = new ArrayList<>();
+
+        // Create an {@link AndroidFlavorAdapter}, whose data source is a list of
+        // {@link AndroidFlavor}s. The adapter knows how to create list item views for each item
+        // in the list.
+        TouristLocationAdapter locationAdapter = new TouristLocationAdapter(this, touristLocations);
+
+        // Get a reference to the ListView, and attach the adapter to the listView.
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(locationAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                TouristLocation location = touristLocations.get(position);
+
+                Uri geoLocation = Uri.parse(location.getLocationCoordinates());
+                showMap(geoLocation);
+            }
+        });
+    }
+
+    public void showMap(Uri geoLocation) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+}
