a7982ad6fd2f97f2330c427d42f81e124e2e45bf
karl-denby <karl.denby@gmail.com>
2016-07-20 19:47:15 +0100

    FamilyActivity >> FamilyFragment

---
+++ app/src/main/java/com/example/android/dublintour/FamilyFragment.java
@@ -0,0 +1,79 @@
+package com.example.android.dublintour;
+
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FamilyFragment extends Fragment {
+
+    public FamilyFragment() {
+        // Required empty public constructor
+        Log.v("FamilyFragment","Constructor Ran");
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.activity_category, container, false);
+
+        // Create an ArrayList of TouristLocation objects
+        final ArrayList<TouristLocation> touristLocations = new ArrayList<>();
+
+        touristLocations.add(new TouristLocation(
+                getString(R.string.family1_name),
+                getString(R.string.family1_desc),
+                getString(R.string.family1_geo),
+                R.drawable.dublin_zoo));
+        touristLocations.add(new TouristLocation(
+                getString(R.string.family2_name),
+                getString(R.string.family2_desc),
+                getString(R.string.family2_geo),
+                R.drawable.wax_museum));
+
+        // Create an {@link AndroidFlavorAdapter}, whose data source is a list of
+        // {@link AndroidFlavor}s. The adapter knows how to create list item views for each item
+        // in the list.
+        TouristLocationAdapter locationAdapter = new TouristLocationAdapter(getActivity(), touristLocations);
+
+        Log.v("FamilyFragment","About to set adapter");
+        // Get a reference to the ListView, and attach the adapter to the listView.
+        ListView listView = (ListView) rootView.findViewById(R.id.listView);
+
+        if (listView != null) {
+            Log.v("FamilyFragment", "try to set Adapter");
+            listView.setAdapter(locationAdapter);
+        } else{
+            Log.v("FamilyFragment", "did not set Adapter");
+        }
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                TouristLocation location = touristLocations.get(position);
+
+                Uri geoLocation = Uri.parse(location.getLocationCoordinates());
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(geoLocation);
+                startActivity(intent);
+            }
+        });
+
+        return rootView;
+    }
+
+}
